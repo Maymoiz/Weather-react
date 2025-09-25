c36c1629b3192b7ef75b582249cb39a52eab2fb9
Maymoiz <208803754+Maymoiz@users.noreply.github.com>
2025-09-25 15:44:22 +0200

    added react

---
+++ my-app/src/weather.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Weather(){
+    return(
+        <div>
+            <h1>My Weather App</h1>
+            <form>
+                <input type="search" />
+                <input type="submit" />
+            </form>
+        </div>
+    )
+}
